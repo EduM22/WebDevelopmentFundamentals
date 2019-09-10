ee2a7ecb7e5b28acb07fab36c1ca6f15e7fdfaba
J <99johanbogg@gmail.com>
2019-09-10 19:42:18 +0200

    login

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
-const db = require('./db')
 const bcrypt = require('bcrypt')
 var helmet = require('helmet')
 const search = require('./search')
+var db = require('./db.js')
+const session = require('express-session')
 
 const port = 8080
 const app = express()
@@ -16,6 +17,15 @@ app.engine('hbs', expressHandlebars({
 app.use(helmet())
 
 app.use(express.static(__dirname + '/public'));  
+
+app.use(session({
+    secret: 'MySuperSecret%&Dsyur7632udhkef478g3fg657i34girew65784frig7w',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: false,
+    }
+}))
 
 app.use(bodyParser.urlencoded({
     extended: false
@@ -47,73 +57,131 @@ app.get('/search', function(req, res) {
     res.render("search.hbs", model)
 })
 
+app.get('/portfolio', function(req, res) {
+    res.send('portfolio')
+})
+
+app.get('/post/:id', function(req, res) {
+    res.send(req.params)
+});
+
+app.get('/post', function(req, res) {
+    res.redirect('/posts')
+});
+
+app.get('/posts', function(req, res) {
+    res.send({'posts': 'allposts'})
+});
+
+app.get('/new/post', isAuthenticated, function(req, res) {
+    res.send('post')
+})
+
+app.post('/new/post', isAuthenticated, function(req, res) {
+    res.send('post')
+})
+
+app.get('/admin', isAuthenticated, function(req, res) {
+    res.send('look at me')
+});
 
 app.get('/login', function(req, res) {
-    res.render('login.hbs', { layout: 'auth.hbs' })
+    res.render('login.hbs', { layout: 'clean.hbs' })
 })
 
 app.post('/login', function(req, res) {
-    const error = ''
-
     const username = req.body.username
     const password = req.body.password
 
     if(username && password) {
-        db.query('SELECT * FROM accounts WHERE username = ?', [username], function(error, results) {
-            if (error) {
-                //no user
-            } else {
-                bcrypt.compare(password ,results.password).then(function(res) {
-                    if (res) {
-                        //login
-                        res.redirect('/admin')
+
+        db.all('SELECT * FROM Users WHERE username = ?', [username], (err, rows) => {
+            console.log(rows);
+            if (rows.length > 0) {
+                bcrypt.compare(password, rows[0].password, function(err, results) {
+                    if (err) {
+                        console.log('err')
                     } else {
-                        //failed login
+                        if (results) {
+                            console.log('jippi')
+                            req.session.authenticated = true
+                            res.redirect('/admin')
+                        } else {
+                            console.log('wrong password')
+                        }
                     }
-                })
+                });
+
+            } else {
+                res.send({}); // failed, so return an empty object instead of undefined
             }
-        })
+        });
+
     } else {
         // no username or password
-        res.render('login.hbs', error)
+        res.render('login.hbs', { layout: 'clean.hbs' })
     }
 })
 
+/*
 
-app.get('/register', function(req, res) {
-    res.render('login.hbs', { layout: 'auth.hbs' })
+app.get('/signup', function(req, res) {
+    res.render('signup.hbs', { layout: 'clean.hbs' })
 })
 
-app.post('/register', function(req, res) {
-    const error = ''
-
+app.post('/signup', function(req, res) {
     const username = req.body.username
-    const password = req.body.password
-    if(username && password) {
-        db.query('SELECT * FROM accounts WHERE username = ?', [username], function(error) {
-            if (error) {
-                bcrypt.hash(password, 12).then(function(hash) {
-                    if (hash) {
-                        // succesful hash
-                        // try db insert
-                        // redirect to login
-                        res.redirect('/login')
-                    } else {
-                        // error with hashing
-                    }
-                })
+
+    if(username && req.body.password) {
+
+        bcrypt.hash(req.body.password, 10, function(err, hash) {
+            if (err) {
+                console.log(err.message)
+                res.status(400).send({'error': 'err'})
             } else {
-                //user already exsists
+                console.log(hash)
+
+                db.run('INSERT INTO Users (username, password, created_date) VALUES (?, ?, ?)', [username, hash, Date.now()], (err) => {
+                    if (err) {
+                        console.log(err.message)
+                        res.redirect('/')
+                    } else {
+                        res.status(200).send({'id': this.lastID})
+                    }
+                });
+
             }
-        })
+        });
+
     } else {
         // no username or password
-        res.render('login.hbs', error)
+        res.render('signup.hbs', { layout: 'clean.hbs' })
     }
 })
 
-app.use(function(req, res, next) {
-    res.status(404).render('404.hbs')
+*/
+
+app.get('/logout', isAuthenticated, function(req, res) {
+    req.session.authenticated = false
+    res.redirect('/')
 });
 
+
+app.use(function(req, res, next) {
+    res.status(404).render('404.hbs', { layout: 'clean.hbs' })
+});
+
+function isAuthenticated(req, res, next) {
+    if (req.session.authenticated) {
+        return next();
+    }
+
+    res.redirect('/')
+}
+
+
 app.listen(port, () => console.log(`App is listening on port ${port}!`))
+
+//db.close();
+
+//user => test,test
--- db.js
+++ db.js
@@ -1,4 +1,81 @@
 var sqlite3 = require('sqlite3').verbose()
-var db = new sqlite3.Database('db-blog.db')
 
-exports.db()
+let db =  new sqlite3.Database('db-blog.db', (error) => {
+    if (error) {
+        console.log(error.message)
+    } else {
+        db.run("CREATE TABLE IF NOT EXISTS Users (\
+            uid INTEGER PRIMARY KEY AUTOINCREMENT,\
+            username CHAR(50) UNIQUE NOT NULL,\
+            password VARCHAR(255) NOT NULL,\
+            created_date DATE NOT NULL)", function(error) {
+            if (error) {
+                console.log(error)
+            }
+        });
+
+
+        db.run("CREATE TABLE IF NOT EXISTS Posts (\
+            postid INTEGER PRIMARY KEY AUTOINCREMENT,\
+            post_user INT NOT NULL,\
+            post_slug CHAR(50) NOT NULL UNIQUE,\
+            post_content TEXT NOT NULL,\
+            post_category char(50) NOT NULL,\
+            post_date DATE NOT NULL,\
+            FOREIGN KEY (post_user) REFERENCES Users(uid))", function(error) {
+            if (error) {
+                console.log(error)
+            }
+        });
+
+
+        db.run("CREATE TABLE IF NOT EXISTS Comments (\
+            commentid INTEGER PRIMARY KEY AUTOINCREMENT,\
+            post_id INT NOT NULL,\
+            comment_username CHAR(50) NOT NULL,\
+            comment_content VARCHAR(255) NOT NULL,\
+            comment_date DATE NOT NULL,\
+            FOREIGN KEY (post_id) REFERENCES Posts(postid))", function(error) {
+            if (error) {
+                console.log(error)
+            }
+        });
+            
+
+        db.run("CREATE TABLE IF NOT EXISTS Pages (\
+            id INTEGER PRIMARY KEY AUTOINCREMENT,\
+            page CHAR(15) NOT NULL,\
+            content TEXT NOT NULL)", function(error) {
+            if (error) {
+                console.log(error)
+            }
+        });
+
+
+        db.run("CREATE TABLE IF NOT EXISTS Guestbook (\
+            id INTEGER PRIMARY KEY AUTOINCREMENT,\
+            name CHAR(50) NOT NULL,\
+            content VARCHAR(255) NOT NULL,\
+            created_date DATE NOT NULL)", function(error) {
+            if (error) {
+                console.log(error)
+            }
+        });
+            
+
+        db.run("CREATE TABLE IF NOT EXISTS Contact (\
+            id INTEGER PRIMARY KEY AUTOINCREMENT,\
+            mail VARCHAR(255) UNIQUE NOT NULL,\
+            content TEXT NOT NULL,\
+            created_date DATE NOT NULL)", function(error) {
+            if (error) {
+                console.log(error)
+            }
+        });
+
+
+
+    }
+});
+
+module.exports = db
